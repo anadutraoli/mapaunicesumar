0b29fa8ef7713ad5589f806a15aa4166e55debb6
Ana Maria Dutra de Oliveira <60656902+anadutraoli@users.noreply.github.com>
2022-08-12 21:26:59 -0300

    Add files via upload

---
+++ MAPA/dados.php
@@ -0,0 +1,12 @@
+<?php
+
+	$title = "Marmitas da Dona Rita";
+
+	$marmitas = array(
+		0 => array('id'=>'1', 'title'=> 'Marmita Caseira', 'description' => "Marmita feita do jeitinho brasileiro, aquele jeitinho que todo brasileiro gosta e que já comeu pelo menos uma vez na vida! Acompanhada com arroz, feijão, salada, macarrão alho e óleo e uma mistura da sua preferência!<Br><strong>Preço: R$18,00", 'imagem'=>'image/marmitacaseira.jpg'),
+		1 => array('id'=>'2', 'title'=> 'Marmita de Carne de Panela', 'description' =>  "Marmita para quem ama uma carne de panela! Acompanhada com uma deliciosa e temperada carne de palena, batata cozida, arroz e feijão!<Br><strong>Preço: R$30,00", 'imagem'=>'image/marmitacarne2.jpg'),
+		2 => array('id'=>'3',  'title'=> 'Marmita Fit', 'description' => "A Marmita Fit é feita especialmente para quem quer comer uma comida gostosa mas não quer sair da dieta, acompanhada com filé de frango, arroz integral e salada e uma fruta da sua preferência!<Br><strong>Preço: R$30,00", 'imagem'=>'image/marmitafit2.jpg'),
+		3 => array('id'=>'4',  'title'=> 'Marmita com Legumes', 'description' => "A Marmita com Legumes é aquela marmita feita para você que AMA legumes e o melhor é que você pode escolher quais legumes você quer e além disso, ela também vem acomoanhada com arroz, feijão e uma mistura da sua prefêrencia, opcional<Br><strong>Preço: R$35,00", 'imagem'=>'image/marmitalegumes2.jpg'),
+		4 => array('id'=>'5',  'title'=> 'Marmita Strogonoff', 'description' => "A mamrita com Strogonoff é perfeit pra quem ama uma strogonoff bem feito e bem temperado! Acompanhado com arozz e com o strogonoff da sua preferência (frango ou carne)!<Br><strong>Preço: R$40,00", 'imagem'=>'image/marmitastrog2.jpg')
+	);
+
